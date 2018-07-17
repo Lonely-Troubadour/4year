eb810a376bdd81fd121624a39f1f1e0976ce8c2e
Luty <1774243057@qq.com>
2018-07-17 08:48:12 +0800

    first commit

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,14 @@
+
+<?php
+
+namespace app\admin\controller;
+
+class Index
+{
+	public function index()
+	{
+		return 'admin';
+		//dump($_ENV);
+		//dump(config());
+	}
+}
---
+++ conf/config.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'app_status'		=> 'dev',
+	'url_route_on'		=> true
+	//'auto_bind_module'	=> true
+];
